38c314c455f9302f1eccd8541a741388fc7f5c3c
Alejandro Mujica <alejandro.mujica@iesdoctorbalmis.com>
2020-11-07 16:01:45 +0100

    Creación dinamicar de los números de la calculadora

--- CalculadoraGrid/MainWindow.xaml.cs
+++ CalculadoraGrid/MainWindow.xaml.cs
@@ -14,7 +14,57 @@ namespace CalculadoraGrid
         public MainWindow()
         {
             InitializeComponent();
+
+            CrearBotones();
+            CrearBotonCero();
         }
 
+        private void CrearBotones()
+        {
+            Button boton;
+            int numero = 1;
+            for (int i = 1; i < CalculadoraGrid.RowDefinitions.Count - 1; i++)
+            {
+                for (int j = 0; j < CalculadoraGrid.ColumnDefinitions.Count; j++)
+                {
+                    boton = new Button();
+                    TextBlock text = new TextBlock();
+                    text.Text = numero.ToString();
+                    Viewbox vb = new Viewbox();
+                    vb.Child = text;
+                    boton.Content = vb;
+                    boton.Tag = numero;
+                    boton.Margin = new Thickness(5);
+                    boton.Style = (Style)Application.Current.Resources["botonesEvent"];
+
+                    numero++;
+
+                    Grid.SetRow(boton, i);
+                    Grid.SetColumn(boton, j);
+
+                    CalculadoraGrid.Children.Add(boton);
+                }
+            }
+        }
+
+        private void CrearBotonCero()
+        {
+            Button boton = new Button();
+            TextBlock text = new TextBlock();
+            text.Text = 0.ToString();
+            Viewbox vb = new Viewbox();
+            vb.Child = text;
+            boton.Content = vb;
+            boton.Tag = 0;
+            boton.Margin = new Thickness(5);
+            boton.Style = (Style)Application.Current.Resources["botonesEvent"];
+            boton.FontSize = 20;
+
+            Grid.SetRow(boton, 4);
+            Grid.SetColumn(boton, 0);
+            Grid.SetColumnSpan(boton, 3);
+
+            CalculadoraGrid.Children.Add(boton);
+        }
     }
 }
